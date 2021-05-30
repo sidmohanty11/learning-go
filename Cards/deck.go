49f4f8bbe03c2be2f951580df511aca5c0f2046a
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-05-31 00:39:21 +0530

    randomising or shuffling part

--- Cards/deck.go
+++ Cards/deck.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 //create a new type of deck -> slice of strings
@@ -53,4 +56,26 @@ func (d deck) deckToString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.deckToString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPos := r.Intn(len(d) - 1)
+
+		d[i], d[newPos] = d[newPos], d[i] //one line swap
+	}
 }
--- Cards/main.go
+++ Cards/main.go
@@ -2,6 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-
-	cards.saveToFile("my_cards")
+	cards.shuffle()
+	cards.print()
 }
