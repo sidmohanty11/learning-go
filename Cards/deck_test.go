046abc934c249e31d2165b98dd869a740d1fcf83
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-05-31 11:11:52 +0530

    struggling with an error

---
+++ Cards/deck_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) { //t -> test handler
+	d := newDeck() //can't import function from another file
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck len of 52 but got, %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades got %v", d[0])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck() //can't import function from another file
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting") //can't import function from another file
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
